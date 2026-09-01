f76123cee20bee39d1c365f11a5ff0c20452f203
Ben <benheaton@live.co.uk>
2026-09-01 10:59:12 +0100

    data loaded onto dev sqlite

--- uaplotter_root/sightings/models.py
+++ uaplotter_root/sightings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models (for 'sightings' app) here.
+
+class Sightings(models.Model):
+    date = models.DateField()
+    decade = models.CharField(max_length=5)
+    time = models.TimeField()
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    shape = models.CharField(max_length=150)
+    summary = models.CharField(max_length=150)
+    media = models.CharField(max_length=1)
+    link = models.URLField()
